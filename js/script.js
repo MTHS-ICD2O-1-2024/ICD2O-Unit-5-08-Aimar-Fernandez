5313ac81c12ff05d1bbb4c8699b0e22c4cb7f5f5
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-05-08 17:28:15 +0000

    added template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Aimar Fernandez
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  let answer = 0
+}
